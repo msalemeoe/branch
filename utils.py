660a596e2443704f754c24c93a587e0d82a93f7e
msalemeoe <mohamedsalem@eyeofegypt.co>
2024-08-07 15:08:19 +0300

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,6 @@
+def format_size(size_in_bytes):
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+        if size_in_bytes < 1024:
+            return f"{size_in_bytes:.2f} {unit}"
+        size_in_bytes /= 1024
+    return f"{size_in_bytes:.2f} PB"
